6b21e918b4e884cb6b0684d474f48149ceb50df0
iamguri-30 <gk5487942@gmail.com>
2023-10-27 22:16:46 +0530

    change react msg

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn  with me
+          Learn react  with me
         </a>
       </header>
     </div>
