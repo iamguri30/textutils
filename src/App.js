2dd6a22ac426aaf623e0bd637e04315f543253f1
iamguri-30 <gk5487942@gmail.com>
2023-10-28 08:12:42 +0530

    change appJS

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-        </p>
           Learn react  with gurleen 
       </header>
     </div>
