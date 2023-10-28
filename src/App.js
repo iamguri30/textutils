b9f0a5194346aa5c7c2bc502171b70b91af6acd7
iamguri-30 <gk5487942@gmail.com>
2023-10-28 08:18:32 +0530

    appJs udated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          Learn react  with gurleen 
+          Learn react  with gurleen  kaur
       </header>
     </div>
   );
