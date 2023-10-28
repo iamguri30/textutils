49d7bf2dba823f370dd0e01cb3c04b7ef39b470d
iamguri-30 <gk5487942@gmail.com>
2023-10-28 08:21:49 +0530

    my msg

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          Learn react  with gurleen  kaur
+          MY REACT APP OF TEXTUTILS
       </header>
     </div>
   );
